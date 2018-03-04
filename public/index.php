89519752421859c097ee3951e1fc2766dbb4ebc9
Bruno Silva <brunocs86@users.noreply.github.com>
2018-03-04 18:49:39 -0300

    App public

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bcs
+ * Date: 19/02/2018
+ * Time: 18:32
+ */
+
+require_once "../vendor/autoload.php";
+
+$route = new \App\Route;
+
+//echo $route->getUrl();
